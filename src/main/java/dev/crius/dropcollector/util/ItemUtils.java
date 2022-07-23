7074f33b04b10dda947cb24ff75ab812742a135c
efekurbann <efekurbann@gmail.com>
2022-07-24 01:41:49 +0300

    Remove debug for the item key

--- src/main/java/dev/crius/dropcollector/util/ItemUtils.java
+++ src/main/java/dev/crius/dropcollector/util/ItemUtils.java
@@ -1,6 +1,5 @@
 package dev.crius.dropcollector.util;
 
-import dev.crius.dropcollector.DropCollectorPlugin;
 import dev.crius.dropcollector.xseries.XMaterial;
 import org.bukkit.Material;
 
@@ -9,9 +8,7 @@ public class ItemUtils {
     public static String getKey(Material material) {
         if (!XMaterial.supports(12)) return null;
 
-        String key = (material.isItem() ? "item." : "block.") + "minecraft." + material.getKey().getKey();
-        DropCollectorPlugin.getInstance().debug(key);
-        return key;
+        return (material.isItem() ? "item." : "block.") + "minecraft." + material.getKey().getKey();
     }
 
 }
